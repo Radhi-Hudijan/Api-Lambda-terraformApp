94332f75d8049ef04783c40040b07b6ceefafad0
Radhi Hudijan <radhif2009@gmail.com>
2024-06-07 18:16:32 +0200

    qoute lambda

--- main.tf
+++ main.tf
@@ -92,6 +92,27 @@ resource "aws_s3_object" "qoute_generator_lambda" {
     source = data.archive_file.qoute_generator_lambda.output_path
 }
 
+resource "aws_lambda_function" "qoute_generator_lambda" {
+    function_name = "qoute-generator-lambda"
+    s3_bucket = aws_s3_bucket.lambda_bucket.bucket
+    s3_key = aws_s3_object.qoute_generator_lambda.key
+    handler = "qoute-generator.lambda_handler"
+    runtime = "python3.8"
+    role = aws_iam_role.lambda_execution_role.arn
+    timeout = 10
+    memory_size = 128
+}
+
+resource "aws_cloudwatch_log_group" "qoute_generator_lambda" {
+    name = "/aws/lambda/qoute-generator-lambda"
+    retention_in_days = 14
+  
+}
+
+
+#Api Geteway
+
+
 
 
 
