7902cbb5ff6537801ef2ead98174642308c21c0c
Radhi Hudijan <radhif2009@gmail.com>
2024-06-07 17:02:11 +0200

    adding lambda code

--- main.tf
+++ main.tf
@@ -35,3 +35,53 @@ resource "aws_s3_object" "health_check_lambda" {
     key = "health-check.zip"
     source = data.archive_file.health_check_lambda.output_path
 }
+
+
+resource "aws_lambda_function" "health_check_lambda" {
+    function_name = "health-check-lambda"
+    s3_bucket = aws_s3_bucket.lambda_bucket.bucket
+    s3_key = aws_s3_object.health_check_lambda.key
+    handler = "health-check.lambda_handler"
+    runtime = "python3.8"
+    role = aws_iam_role.lambda_execution_role.arn
+    timeout = 10
+    memory_size = 128
+}
+
+resource "aws_cloudwatch_log_group" "health_check_lambda" {
+    name = "/aws/lambda/health-check-lambda"
+    retention_in_days = 14
+  
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+    name = "lambda-role"
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+    role = aws_iam_role.lambda_execution_role.name
+  
+}
+
+
+
+
+
+
+
+
+
+
