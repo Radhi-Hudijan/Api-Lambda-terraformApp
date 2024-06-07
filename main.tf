80f98d4298786096f46fa0775e7a83a7380293eb
Radhi Hudijan <radhif2009@gmail.com>
2024-06-07 18:56:47 +0200

    updating api integration and routes

--- main.tf
+++ main.tf
@@ -112,12 +112,54 @@ resource "aws_cloudwatch_log_group" "qoute_generator_lambda" {
 
 #Api Geteway
 
+resource "aws_apigatewayv2_api" "serverless_lambda" {
+    name = "serverless_HTTP_GW"
+    protocol_type = "HTTP"
+}
 
 
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.serverless_lambda.id
+  name = "Dev_stage"
+}
+
+## integration and route for health check function
+
+resource "aws_apigatewayv2_integration" "health" {
+  description = "Health chcek function integration"
+  api_id= aws_apigatewayv2_api.serverless_lambda.id
+  integration_uri    = aws_lambda_function.health_check_lambda.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "health_route" {
+    api_id = aws_apigatewayv2_api.serverless_lambda.id
+    route_key = "Get/health"
+    target = "integrations/${aws_apigatewayv2_integration.health.id}"
+}
 
 
+## integration and route for qoute generator function
+resource "aws_apigatewayv2_integration" "qoute_generator" {
+    description = "qoute generator integration"
+    api_id = aws_apigatewayv2_integration.serverless_lambda.id
+    integration_uri = aws_lambda_function.qoute_generator_lambda.invoke_arn
+    integration_type = "AWS_PROXY"
+    integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "qoute_route" {
+    api_id = aws_apigatewayv2_api.serverless_lambda.id
+    route_key = "GET/qoute"
+    target = "integrations/${aws_apigatewayv2_integration.qoute_generator.id}"
+  
+}
 
 
+# resource "aws_lambda_permission" "invoke_lambda" {
+  
+# }
 
 
 
